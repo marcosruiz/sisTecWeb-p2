bbd14c6d42196ba7cfbccf6d0dd8babf26e0a138
Marcos Ruiz Garcia <sobrenombre@gmail.com>
2016-08-12 20:10:34 +0200

    Done :S
    en consola pone un data= y no estoy seguro de si es concurrente

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var server = require("./server");
 var router = require("./router");
-var requestHandlers = require("./requestHandlers" );
+var requestHandlers = require("./requestHandlers");
 
 var handle = {};
-handle["/" ] = requestHandlers.start;
-handle["/start" ] = requestHandlers.start;
-handle["/upload" ] = requestHandlers.upload;
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
 
 server.start(router.route, handle);
--- requestHandlers.js
+++ requestHandlers.js
@@ -2,7 +2,7 @@ var exec = require("child_process").exec;
 
 function start(response) {
 	console.log("Request handler 'start' was called.");
-	
+
 	exec("dir", function (error, stdout, stderr) {
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write(stdout);
@@ -11,10 +11,16 @@ function start(response) {
 }
 function upload(response) {
 	console.log("Request handler 'upload' was called.");
-	
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello Upload");
+
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write("<form method='post'>");
+	response.write("<textarea type='text' name='data' cols='50' rows='10'></textarea>");
+	response.write("<br/>");
+	response.write("<button type='submit'>Send</button>");
+	response.write("</form>");
 	response.end();
+
+
 }
 exports.start = start;
 exports.upload = upload;
